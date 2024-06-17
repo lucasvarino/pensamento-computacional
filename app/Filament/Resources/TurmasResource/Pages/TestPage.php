c8b5f491f8e67963b694c3aef5b355a5a1e71a48
lucasvarino <lucasvarino@gmail.com>
2024-06-17 08:36:47 -0300

    Sending e-mail when test is completed

--- app/Filament/Resources/TurmasResource/Pages/TestPage.php
+++ app/Filament/Resources/TurmasResource/Pages/TestPage.php
@@ -11,11 +11,13 @@ use App\Models\Question;
 use App\Models\State;
 use App\Models\TestClass;
 use Carbon\Carbon;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Notifications\Actions\Action;
@@ -23,6 +25,7 @@ use Filament\Notifications\Notification;
 use Filament\Resources\Pages\Page;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class TestPage extends Page
 {
@@ -62,7 +65,7 @@ class TestPage extends Page
                 'state' => $states['state']
             ]);
 
-            $answers = array_filter($states, fn ($key) => $key !== 'name' && $key !== 'age' && $key !== 'gender' && $key !== 'state', ARRAY_FILTER_USE_KEY);
+            $answers = array_filter($states, fn ($key) => $key !== 'name' && $key !== 'age' && $key !== 'gender' && $key !== 'state' && $key !== 'sendEmail' && $key !== 'email', ARRAY_FILTER_USE_KEY);
 
             foreach ($answers as $response) {
                 AnswerClass::create([
@@ -83,7 +86,10 @@ class TestPage extends Page
                     return ['group_id' => $groupId, 'value' => $value, 'answer_id' => $answer->id];
                 })->map(fn (array $arr) => BartleResult::create($arr));
 
-            $resultArray = $results->toArray();
+            if ($states['sendEmail'] && $states['email']) {
+                Mail::to($states['email'])
+                    ->send(new \App\Mail\TestResult());
+            }
 
             // Notify the owner of the class
             Notification::make()
@@ -138,7 +144,15 @@ class TestPage extends Page
                             ->searchable()
                             ->required()
                             ->native(false)
-                            ->label('Estado')
+                            ->label('Estado'),
+                        Checkbox::make('sendEmail')
+                            ->label('Enviar resultado do teste por e-mail?')
+                            ->live(),
+                        TextInput::make('email')
+                            ->label('E-mail')
+                            ->email()
+                            ->required(fn (Get $get): bool => $get('sendEmail'))
+                            ->visible(fn (Get $get): bool => $get('sendEmail')),
                     ])->columns(2),
                 Section::make('Questionário')
                     ->description('Responda o questionário e em seguida mostraremos o resultado')
--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'mailersend'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,6 +68,10 @@ return [
         'sendmail' => [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
+        ],
+
+        'mailersend' => [
+            'transport' => 'mailersend',
         ],
 
         'log' => [
