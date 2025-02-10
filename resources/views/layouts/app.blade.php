8e9d0ff37fb86266273d93aa07602c560ec44e53
Vit0rGomes <vitor.gomes@estudante.ufjf.br>
2025-02-10 15:43:26 -0300

    changing all title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'GamePersona') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
