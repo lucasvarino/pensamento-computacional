cc984c3cce5e5418b6e2dbb8a55c4d488e602a71
Vitor Gomes <vitor.gomes@estudante.ufjf.br>
2025-03-23 19:07:04 -0300

    fiz test expiration bug

--- resources/views/filament/resources/turmas-resource/pages/test-page.blade.php
+++ resources/views/filament/resources/turmas-resource/pages/test-page.blade.php
@@ -1,18 +1,20 @@
 <x-filament-panels::page>
     <div>
-        @if(!$this->expirated)
-            <form wire:submit="create">
-                {{ $this->form }}
-                <div class="mt-6">
-                    <x-filament::button type="submit">
-                        Enviar
-                    </x-filament::button>
-                </div>
-            </form>
-        @else
-            <div class="mx-auto text-center flex items-center justify-center">
+        @if($this->expirated)
+        <div class="mx-auto text-center flex items-center justify-center">
                 <h1 class="font-bold">Este teste já foi encerrado, entre em contato com o professor responsável.</h1>
+        </div>
+        
+        @else
+
+        <form wire:submit="create">
+            {{ $this->form }}
+            <div class="mt-6">
+                <x-filament::button type="submit">
+                    Enviar
+                </x-filament::button>
             </div>
+        </form>
         @endif
     </div>
 </x-filament-panels::page>
