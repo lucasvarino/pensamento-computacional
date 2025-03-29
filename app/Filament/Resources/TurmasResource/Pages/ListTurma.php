78abe73ef12c794c757664171494fa57b7100091
Vitor Gomes <vitor.gomes@estudante.ufjf.br>
2025-03-28 23:14:34 -0300

    new turma resouce acess

--- app/Filament/Resources/TurmasResource/Pages/ListTurma.php
+++ app/Filament/Resources/TurmasResource/Pages/ListTurma.php
@@ -5,19 +5,39 @@ namespace App\Filament\Resources\TurmasResource\Pages;
 use App\Filament\Resources\TurmaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Notifications\Notification;
 
 class ListTurma extends ListRecords
 {
     protected static string $resource = TurmaResource::class;
+    public $previousUrl;
 
     public function mount(): void
     {
-        if (!auth()->user()) {
-            $this->redirect('/login');
+        $user = auth()->user();
+        $verified = $user && $user->isVerified() ? true : false;
+
+        if (!$user) {
+            Notification::make()
+            ->title('Acesso negado')
+            ->body('Você precisa estar autenticado e verificado para acessar esta página.')
+            ->danger()
+            ->send();
+
+            redirect()->back();
         }
 
-        if (!auth()->user()->isVerified()) {
-            $this->redirect('/admin');
+        if (!$verified) {
+
+            $this->previousUrl = url()->previous();
+
+            Notification::make()
+            ->title('Acesso negado')
+            ->body('Você precisa estar autenticado e verificado para acessar esta página.')
+            ->danger()
+            ->send();
+
+            redirect($this->previousUrl);
         }
     }
 
