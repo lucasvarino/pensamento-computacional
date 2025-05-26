9088f7679f494cd785dd19f1920fa8af00da3401
Vitor Gomes <vitor.gomes@estudante.ufjf.br>
2025-05-25 22:59:11 -0300

    mail and pdf assignature

--- resources/views/mail/test/result.blade.php
+++ resources/views/mail/test/result.blade.php
@@ -1,18 +1,18 @@
 <div>
         <div class="w-full max-w-3xl mx-auto px-4">
             <p>
-                Olá, Parabéns por concluir o Teste de Perfil de Jogador - Game Persona! 
-                Abaixo você encontra o relatório com suas respostas e a análise do seu perfil de jogador, 
-                desenvolvido pelo projeto de Perfil de Jogador da UFJF. Este documento apresenta uma visão 
-                personalizada de como você interage com jogos, destacando suas motivações, 
-                estilos de jogo predominantes e recomendações para potencializar sua experiência lúdica.
-                Obrigado pela sua participação!
+            Olá,<br>
+            Parabéns por concluir o seu Teste de Perfil de Jogador! <br>
+            Abaixo, você encontra o relatório com suas respostas e a análise do seu perfil. <br>
+            Este relatório traz uma visão personalizada de como você se envolve em diferentes atividades,
+            destacando suas motivações, estilos predominantes e sugestões para aprimorar seu engajamento. <br>
+            Obrigado pela sua participação!
             </p>
-            <h2 class="text-2xl dark:text-white font-semibold text-center mt-8">Resultados do Teste {{ $className ?? '' }}</h2>
+            <h2 class="text-2xl dark:text-white font-semibold text-center mt-8">Resultados do Teste de Perfil de Jogador</h2>
             <div class="grid md:flex md:flex-col-reverse md:gap-4">
                 <div class="bg-gray-100 shdc-shadow-2 rounded-lg overflow-hidden flex flex-wrap items-center justify-center">
                     <div class="border-t border-gray-200 w-full"></div>
-                    <div class="grid gap-4 p-6">
+                    <div class="grid gap-3 p-6">
                         @foreach($results as $result)
                             <div class="flex items-center">
                                 <h3 class="text-lg font-bold">{{ $result['group'] }}: {{ $result['value'] }}%</h3>
@@ -24,5 +24,18 @@
                         @endforeach
                 </div>
             </div>
+            <p>
+                Atenciosamente, <br>
+                Equipe Game Persona <br>
+                Projeto orientado por Prof. Eduardo Barrére<br>
+                DCC / ICE / UFJF<br>
+                Contato:<br>
+                projeto.perfiljogador@ufjf.br<br>
+                Site:<br>
+                https://perfiljogador.repesq.ufjf.br<br>
+                ------------------------------<br>
+                Este relatório pode conter informações confidenciais.  <br>
+                Se você recebeu esta mensagem por engano, por favor informe para projeto.perfiljogador@ufjf.br e apague-a.
+        </p>
         </div>
 </div>
