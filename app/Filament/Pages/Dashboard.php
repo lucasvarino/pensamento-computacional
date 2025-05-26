375b8903c70c7680142ebef7e3b03e4e5001c14a
Vitor Gomes <vitor.gomes@estudante.ufjf.br>
2025-05-25 22:58:17 -0300

    new dashboard redirect for not auth users

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,16 +2,34 @@
 
 namespace App\Filament\Pages;
 use Filament\Facades\Filament;
+use Filament\Notifications\Notification;
+
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
-    protected static ?string $title = 'Painel';
+    // protected static ?string $title = 'Painel';
 
     public static function getNavigationIcon(): ?string
     {
         return request()->routeIs('filament.admin.pages.dashboard')
             ? 'icon-home'
             : 'icon-home-b&w';
+    }
+
+    public function mount(): void
+    {
+        $user = auth()->user();
+
+        if (!$user) {
+            $this->previousUrl = url()->previous();
+            Notification::make()
+            ->title('Acesso negado')
+            ->body('Você precisa estar autenticado para acessar esta página.')
+            ->danger()
+            ->send();
+
+            redirect($this->previousUrl);
+        }
     }
 
 }
