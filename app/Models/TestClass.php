63161727ad1c35f390743ff6c0af81b319096642
Vitor Gomes <vitor.gomes@estudante.ufjf.br>
2026-01-12 22:23:41 -0300

    validator of term and expire_date

--- app/Models/TestClass.php
+++ app/Models/TestClass.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Validator;
 
 class TestClass extends Model
 {
@@ -19,5 +20,32 @@ class TestClass extends Model
     public function method()
     {
         return $this->belongsTo(Method::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(function ($model) {
+            $data = [
+                'term' => $model->term,
+                'expire_date' => $model->expire_date,
+            ];
+
+            Validator::make($data, [
+                'term' => ['accepted'],
+                'expire_date' => ['nullable', 'date', 'after_or_equal:today'],
+            ])->validate();
+        });
+
+        static::updating(function ($model) {
+            $data = [
+                'term' => $model->term,
+                'expire_date' => $model->expire_date,
+            ];
+
+            Validator::make($data, [
+                'term' => ['accepted'],
+                'expire_date' => ['nullable', 'date', 'after_or_equal:today'],
+            ])->validate();
+        });
     }
 }
